c43de4f3561d2c94dd3a4f08c20e9e788466c2bf
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2022-02-17 11:35:13 +0100

    Test update #patch

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -9,6 +9,12 @@
         public function testConfig_construct(){
             $config = new \Tchevalleraud\ExtremeNetworks\XIQSE\Config("192.168.1.1", "bhkemlwVvj", "53e55769-50c8-483d-9665-40e497dc03fc");
             $this->assertEquals(new \Tchevalleraud\ExtremeNetworks\XIQSE\Config("192.168.1.1", "bhkemlwVvj", "53e55769-50c8-483d-9665-40e497dc03fc"), $config);
+
+            $this->assertEquals("192.168.1.1", $config->getServer());
+            $this->assertEquals("bhkemlwVvj", $config->getClientId());
+            $this->assertEquals("53e55769-50c8-483d-9665-40e497dc03fc", $config->getClientSecret());
+            $this->assertEquals("https", $config->getProtocol());
+            $this->assertEquals("8443", $config->getPort());
         }
 
     }
